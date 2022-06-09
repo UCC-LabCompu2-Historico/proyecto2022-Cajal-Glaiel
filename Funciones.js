72d34538fc1b2c598cdcf3cee174e21cae43696b
Agustin Glaiel <agustinglaiel@Agustins-MacBook-Pro.local>
2022-06-09 10:23:23 -0300

    a

--- Funciones.js
+++ Funciones.js
@@ -3,3 +3,8 @@ function AbrirJuego(){
     url = "Juego.html";
     window.open(url);
 }
+
+function DibujarCanvas() {
+    var canvas = document.getElementById("Canvas");
+    var ctx = canvas.getContext("2d");
+}
