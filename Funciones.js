589f5df1c7d7cd94de9a34df6dd0420eb5ee9e5d
Agustin Glaiel <agustinglaiel@Agustins-MacBook-Pro.local>
2022-06-13 11:48:22 -0300

    funciones

--- Funciones.js
+++ Funciones.js
@@ -1,12 +1,3 @@
-document.onload = cargarFunciones();
-
-function cargarFunciones(){
-    Juguemos();
-    Segundero();
-    SumarPuntaje();
-}
-
-
 function AbrirJuego(){
     let url;
     url = "Juego.html";
@@ -26,34 +17,34 @@ function FunAyudas() {
 }
 
 function Segundero(){
-    segundos = 0;
-    minutos = 0;
-    s = document.getElementById("Segundos");
-    m = document.getElementById("minutos");
+    segundos=0;
+    minutos=0
+    s= document.getElementById("segundos");
+    m=document.getElementById("minutos")
 
-    tiempero = setInterval(
-        function (){
-            if (segundos==60){
-                segundos = 0;
+    tiempero=setInterval(
+        function(){
+            if(segundos==60){
+                segundos=0;
                 minutos++;
-                m.innerHTML = minutos;
-                if (minutos==60){
-                    minutos = 0;
+                m.innerHTML=minutos;
+                if(minutos==60){
+                    minutos=0;
                 }
             }
-            s.innerHTML = segundos;
-            segundos++;
-        }, 100);
+            s.innerHTML=segundos;
+            segundos++
+        },1000 );
     console.log(tiempero)
 }
 
 function Juguemos(){
-    let min = 1;
-    let max = 10;
-    let numero1 = Math.random()*(max - min)+min;
-    let numero2 = Math.random()*(max - min)+min;
-    let n1 = Math.trunc(numero1);
-    let n2 = Math.trunc(numero2);
+    let min =1;
+    let max=10;
+    let numero1=Math.random()*(max-min)+min;
+    let numero2=Math.random()*(max-min)+min;
+    let n1=Math.trunc(numero1) //saco los decimales
+    let n2=Math.trunc(numero2)//saco los decimales
     document.getElementById("num1").innerHTML = n1;
     document.getElementById("num2").innerHTML = n2;
     let correcto = numero1 * numero2;
@@ -61,18 +52,18 @@ function Juguemos(){
 }
 
 function SumarPuntaje () {
-    let puntaje = o;
-    let n1 = document.getElementById("num1").innerHTML;
-    let n2 = document.getElementById("num2").innerHTML;
-    let correcto = n1 * n2;
-    console.log(correcto);
+    let puntaje=0;
+    let n1=document.getElementById("num1").innerHTML;
+    let n2=document.getElementById("num2").innerHTML;
+    let correcto=n1*n2;
+    console.log(correcto)
     let respuesta = document.getElementById("num3").value;
-    if (correcto == respuesta){
-        puntaje += 10;
+    if(correcto==respuesta){
+        puntaje+=10
+    }else{
+        alert("Respuesta incorrecta")
     }
-    else {
-        alert("Respuesta Incorrecta")
-    }
+
     document.getElementById("puntaje").outerHTML = puntaje;
-    Juguemos();
+    Juguemos()
 }
